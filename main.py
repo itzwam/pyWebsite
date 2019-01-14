b49d6b5067200df16b8a6567d602f3fae41f4c3a
iTzWam <antoine@MacBookPro-Antoine>
2019-01-14 12:51:50 +0100

    Stuff

--- main.py
+++ main.py
@@ -27,6 +27,11 @@ def getentry(code):
     myresult = mycursor.execute("SELECT id, code, description FROM info where code = '{}'".format(code))
     mydb.close()
     result = myresult[0]
+  return {
+    'id' : result[0],
+    'code' : result[1],
+    'description' : result[2],
+  }
   except IndexError:
     return {
       'error': True,
@@ -34,11 +39,6 @@ def getentry(code):
     }
   except Exception as e:
     pass
-  return {
-    'id' : myresult[0],
-    'code' : myresult[1],
-    'description' : myresult[2],
-  }
 
 def addentry(code, description):
   try:
